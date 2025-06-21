bcee2aae8859fc6cd4a6a525a0decf6492f50687
johnSmitt <smittjohn.levelup@gmail.com>
2025-06-21 23:50:07 +0200

    feat: rewrote logic, added MAX_SUPPLY and mint cap check

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,14 +1,77 @@
 // SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.28;
 
-contract MyContract {
-    uint256 public value;
-    
-    function setValue(uint256 _value) public {
-        value = _value;
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC1363} from "@openzeppelin/contracts/token/ERC20/extensions/ERC1363.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract BestOfTheBest is ERC20, ERC20Burnable, ERC20Pausable, AccessControl, ERC1363, ERC20Permit {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(address defaultAdmin, address pauser, address minter)
+        ERC20("BestOfTheBest", "BOB")
+        ERC20Permit("BestOfTheBest")
+    {
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(PAUSER_ROLE, pauser);
+        _grantRole(MINTER_ROLE, minter);
     }
 
-    function getValue() public view returns (uint256) {
-        return value;
+    mapping(address => bool) public blacklisted;
+
+
+    event Minted(address indexed to, uint256 amount);
+    event Paused(address by);
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
     }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        require(totalSupply() + amount <= MAX_SUPPLY, "Cap exceeded");
+        _mint(to, amount);
+        emit Minted(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable)
+    {
+        super._update(from, to, value);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(AccessControl, ERC1363)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    
+
+    function setBlacklist(address account, bool value) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        blacklisted[account] = value;
+    }
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable)
+    {
+        require(!blacklisted[from] && !blacklisted[to], "Blacklisted");
+        super._update(from, to, value);
+    }
+
 }
